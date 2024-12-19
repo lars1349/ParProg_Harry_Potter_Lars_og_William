42830921b14b5006766ba2850685b321fa07c7f3
lars1349 <mostuelars@gmail.com>
2024-12-19 15:00:13 +0100

    Update and rename Shop.cs to MagicShop.cs

---
+++ MagicShop.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParProg_Harry_Potter_Lars_og_William
+{
+    internal class MagicShop
+    {
+        public MagicShop(Wizard wizard)
+        {
+            Wizard = wizard;
+        }
+
+        public Wizard Wizard { get; set; }
+
+        private string[] _wands = { "Phoenix Wand", "Unicorn Wand", "Wand of Troll" };
+        private string[] _pets = { "Cat", "Owl", "Rat" };
+
+
+        public void Shop()
+        {
+            Console.WriteLine("What do you want to buy?");
+            Console.WriteLine("[1] Wand \n[2] Pet\n[3] Leave store");
+            var input = Console.ReadLine();
+            switch(input)
+            {
+                case "1":
+                    Wands();
+                    break;
+                case "2":
+                    Pets();
+                    break;
+                case "3":
+                    break;
+            }
+
+        }
+
+        public void Wands()
+        {
+            for (int i = 0; i < _wands.Length; i++)
+            {
+                Console.WriteLine($"[{i + 1}] {_wands[i]}");
+            }
+            var newWand = Convert.ToInt32(Console.ReadLine()) - 1;
+            Wizard.Inventory[0] = _wands[newWand];
+            Shop();
+        }
+
+        public void Pets()
+        {
+            for (int i = 0; i < _pets.Length; i++)
+            {
+                Console.WriteLine($"[{i + 1}] {_pets[i]}");
+            }
+            var newPet = Convert.ToInt32(Console.ReadLine()) - 1;
+            Wizard.Inventory[1] = _pets[newPet];
+            Shop();
+        }
+
+
+    }
+}
